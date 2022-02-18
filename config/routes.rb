eeb601cb8f1cc504bbfcafea186aeebc5855912b
Toby Manley <tobymanley1990@gmail.com>
2022-02-18 11:41:20 +0900

    change platform in gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     resources :bookmarks, only: [:new, :create]
   end # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :bookmarks, only: [:destroy]
+  root to: 'lists#index'
 end
